1e3ce6a2784026f074dea1cee8566f706800967c
Unknown6656 <Unknown6656@users.noreply.github.com>
2025-05-22 01:40:45 +0200

    generated initial version

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,55 @@
+
+//////////////////////////////////////////////////////////////////////////
+// Auto-generated 2025-05-22 01:38:50.475                               //
+// ANY CHANGES TO THIS DOCUMENT WILL BE LOST UPON RE-GENERATION         //
+//////////////////////////////////////////////////////////////////////////
+
+using System.Reflection;
+using System;
+
+[assembly: AssemblyVersion("0.0.2.9042")]
+[assembly: AssemblyFileVersion("0.0.2.9042")]
+[assembly: AssemblyInformationalVersion("v.0.0.2.9042, commit: e57b1a4775e263b479d2105fd54acb7f8aef3dfe")]
+[assembly: AssemblyCompany("Unknown6656")]
+[assembly: AssemblyCopyright("Copyright © 2025, Unknown6656")]
+[assembly: AssemblyProduct("Pythia by Unknown6656")]
+[assembly: AssemblyTitle("autoit3")]
+
+/// <summary>
+/// A global module containing some meta-data.
+/// </summary>
+public static class __module__
+{
+    /// <summary>
+    /// The project's author. This value is equal to the author of the GitHub repository associated with <see cref="RepositoryURL"/>.
+    /// </summary>
+    public const string Author = "Unknown6656";
+    /// <summary>
+    /// Development year(s).
+    /// </summary>
+    public const string Year = "2025";
+    /// <summary>
+    /// The project's copyright information.
+    /// </summary>
+    public const string Copyright = "Copyright © 2025, Unknown6656";
+    /// <summary>
+    /// The project's current version.
+    /// </summary>
+    public static Version? ProgramVersion { get; } = Version.Parse("0.0.2.9042");
+    /// <summary>
+    /// The Git hash associated with the current build.
+    /// </summary>
+    public const string GitHash = "e57b1a4775e263b479d2105fd54acb7f8aef3dfe";
+    /// <summary>
+    /// The name of the GitHub repository associated with <see cref="RepositoryURL"/>.
+    /// </summary>
+    public const string RepositoryName = "Pythia";
+    /// <summary>
+    /// The URL of this project's GitHub repository.
+    /// </summary>
+    public const string RepositoryURL = "https://github.com/Unknown6656/Pythia";
+    /// <summary>
+    /// The date and time of the current build (2025-05-22 01:38:50.475).
+    /// </summary>
+    public static DateTime DateBuilt { get; } = DateTime.FromFileTimeUtc(0x01dbcaa98146e596L);
+}
